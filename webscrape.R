41e04ac0c47eb080d7ec7f7d208db689c9968a86
Dominick J. Lemas <dominicklemas@gmail.com>
2021-06-01 22:43:21 -0400

    add demo code

---
+++ webscrape.R
@@ -0,0 +1,13 @@
+
+# https://www.analyticsvidhya.com/blog/2017/03/beginners-guide-on-web-scraping-in-r-using-rvest-with-hands-on-knowledge/
+
+# install.packages('rvest')
+
+#Loading the rvest package
+library('rvest')
+
+#Specifying the url for desired website to be scraped
+url <- 'http://www.imdb.com/search/title?count=100&release_date=2016,2016&title_type=feature'
+
+#Reading the HTML code from the website
+webpage <- read_html(url)
